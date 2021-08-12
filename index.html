546d9c0afd6801ecbe10becd45decab545598c85
fahmij8 <fahmijabbar@upi.edu>
2021-08-13 02:09:56 +0700

    Fix hack language

---
+++ index.html
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="refresh" content="3; url=https://trainercv-dpte.web.app/" />
+	<title>Restricted Area</title>
+</head>
+<body>
+<p>This page was meant to never founded. But you did!</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="refresh" content="3; url=https://trainercv-dpte.web.app/" />
-	<title>Restricted Area</title>
-</head>
-<body>
-<p>This page was meant to never founded. But you did!</p>
-</body>
-</html>
+<?php include 'index.html';?>
